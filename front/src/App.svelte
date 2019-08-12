b352807d05c4a475ef39bb5ee98cf7b31280725c
Daniel Reszka <dannn.r@gmail.com>
2019-08-13 01:15:40 +0200

    add barebones Apollo client POC in frontend

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -2,7 +2,7 @@
   import Chat from '/Chat/Chat.svelte';
   import Header from '/Header.svelte';
   import UserForm from '/UserForm.svelte';
-
+  import Users from '/Users/Users.svelte';
   let user; //= { age: 18, userName: 'dannnn' };
 </script>
 
@@ -13,6 +13,8 @@
 </style>
 
 <div class="container mx-auto">
+  <Users />
+
   {#if user}
     <Header />
     <Chat {user} />
