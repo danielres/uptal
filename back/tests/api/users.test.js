2891dc4801471984c105c14fa3bce10730509216
Daniel Reszka <dannn.r@gmail.com>
2019-08-19 11:21:30 +0200

    add first basic users api test

---
+++ back/tests/api/users.test.js
@@ -0,0 +1,39 @@
+import request from "supertest";
+
+import server from "../../src/index";
+import execCypher from "../../src/helpers/neo4j/execCypher";
+
+const purgeDb = () => execCypher(`MATCH (n) DETACH DELETE n`);
+
+beforeEach(purgeDb);
+afterEach(purgeDb);
+
+const ADD_USER = /* GraphQL */ `
+  mutation {
+    CreateUser(name: "Bob") {
+      id
+    }
+  }
+`;
+
+const GET_USERS = /* GraphQL */ `
+  {
+    User {
+      name
+      id
+    }
+  }
+`;
+
+test("Creating a user + getting users", async () => {
+  await request(server)
+    .post("/graphql")
+    .send({ query: ADD_USER });
+
+  const resp = await request(server)
+    .post("/graphql")
+    .send({ query: GET_USERS });
+
+  expect(JSON.parse(resp.text).data.User.length).toEqual(1);
+  expect(JSON.parse(resp.text).data.User[0].name).toEqual("Bob");
+});
